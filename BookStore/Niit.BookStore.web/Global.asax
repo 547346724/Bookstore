65b934a57cda885db35f4fe07cd36b7d883c3f56
sky <547346724@qq.com>
2017-03-10 10:18:14 +0800

    创建项目

---
+++ BookStore/Niit.BookStore.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Niit.BookStore.web.MvcApplication" Language="C#" %>
---
+++ BookStore/Niit.BookStore.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Niit.BookStore.web.Startup))]
+namespace Niit.BookStore.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
